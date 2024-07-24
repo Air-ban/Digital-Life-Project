5495695e8677fed42aa5f4db3948ae685228eb0e
xiaoxiao <xiaoxiaofpu@outlook.com>
2024-07-24 19:50:59 +0800

    音频输入更新为可用状态

--- input/voice-input.py
+++ input/voice-input.py
@@ -1,29 +1,39 @@
 import rapid_paraformer as rp
 import sounddevice as sd
 import soundfile as sf
+from funasr import AutoModel
+from funasr.utils.postprocess_utils import rich_transcription_postprocess
+
+model_dir = "FunAudioLLM/SenseVoiceSmall" #指定模型
+#加载模型
+model = AutoModel(
+    model=model_dir,
+    vad_model="fsmn-vad",
+    vad_kwargs={"max_single_segment_time": 30000},
+    #cuda设备，我没有，我也不配
+    #device="cuda:0", 
+    hub="hf",
+)
 
 duration = 5  # 录音时长（秒）
 fs = 44100  # 采样率（Hz）
 channels = 2  # 声道数
 
-# 初始化Paraformer模型
-config_path = "resources_config.yaml"
-paraformer = rp.RapidParaformer(config_path)
-
-# 音频文件路径列表
-wav_paths = [
-    r"temp.wav",
-    # 更多音频文件路径...
-]
-
 # 循环运行Paraformer模型
 while True:
+    res = model.generate(
+    input=r"C:\Users\xiaox\Documents\GitHub\Digital-Life-Project\temp.wav",
+    cache={},
+    language="auto",  # "zn", "en", "yue", "ja", "ko", "nospeech"
+    use_itn=True,
+    batch_size_s=60,
+    merge_vad=True,  #
+    merge_length_s=15,
+)
     recording = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
     sd.wait()  # 等待录音完成
     filename = 'temp.wav'  # 保存的文件名
     sf.write(filename, recording, fs)  # 保存录音
-    for wav_path in wav_paths:
-        result = paraformer(wav_path)
-        print(result)
-
-# 未达到可用预期，需后期优化
+    text = rich_transcription_postprocess(res[0]["text"])
+    print(text)
+#保存这个text为说话的内容，靠你啦！
