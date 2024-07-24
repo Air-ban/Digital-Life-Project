ea79f2c18101c5ed868f10cf84fed6036ac452d1
xiaoxiao <xiaoxiaofpu@outlook.com>
2024-07-25 00:56:03 +0800

    改写为类

--- input/server/date-post.py
+++ input/server/date-post.py
@@ -1,47 +1,49 @@
 import socket
 
-# 设置服务器地址和端口
-SERVER_HOST = '127.0.0.1'  # 使用0.0.0.0可以监听所有可用的网络接口
-SERVER_PORT = 12345       # 服务器端口
+class TCPServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        print(f"服务器监听在 {self.host}:{self.port}")
 
-# 创建一个TCP/IP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# 绑定地址和端口
-server_socket.bind((SERVER_HOST, SERVER_PORT))
-
-# 监听连接
-server_socket.listen(5)
-print(f"服务器监听在 {SERVER_HOST}:{SERVER_PORT}")
-
-try:
-    while True:
-        # 等待客户端连接
-        client_socket, client_address = server_socket.accept()
-        print(f"接受来自 {client_address} 的连接")
+    def start(self):
         try:
-            # 接收数据
             while True:
-                data = client_socket.recv(1024)  # 设置接收数据的大小
-                if not data:
-                    break  # 如果没有数据，则断开连接
-                print(f"收到数据: {data.decode('utf-8')}")
-                with open('rate.txt', 'w', encoding='utf-8') as file:
-                    file.write(data)
-                with open('words.txt', 'w', encoding='utf-8') as file:
-                    file.write(data)
-                response = 'OK'
-                client_socket.sendall(response.encode('utf-8'))
-
+                client_socket, client_address = self.server_socket.accept()
+                print(f"接受来自 {client_address} 的连接")
+                try:
+                    self.handle_client(client_socket)
+                finally:
+                    client_socket.close()
+                    print(f"连接 {client_address} 已关闭")
         finally:
-            # 关闭客户端连接
-            client_socket.close()
-            print(f"连接 {client_address} 已关闭")
-            break
+            self.stop()
 
-finally:
-    # 关闭服务器 socket
-    server_socket.close()
-    print("服务器已关闭")
+    def handle_client(self, client_socket):
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print(f"收到数据: {data.decode('utf-8')}")
+            with open('rate.txt', 'w', encoding='utf-8') as file:
+                file.write(data.decode('utf-8'))
+            with open('words.txt', 'w', encoding='utf-8') as file:
+                file.write(data.decode('utf-8'))
+            response = 'OK'
+            client_socket.sendall(response.encode('utf-8'))
 
-# 欠完善，没有具体解析需求
+    def stop(self):
+        self.server_socket.close()
+        print("服务器已关闭")
+
+if __name__ == "__main__":
+    # 设置服务器地址和端口
+    SERVER_HOST = '127.0.0.1'  # 使用0.0.0.0可以监听所有可用的网络接口
+    SERVER_PORT = 12345        # 服务器端口
+
+    # 创建服务器实例并启动
+    server = TCPServer(SERVER_HOST, SERVER_PORT)
+    server.start()
